6fddf859ab2c9e6bf558d8f20cf4632214740e7f
tgno3 <tgno3.com@gmail.com>
2021-06-01 14:50:25 +0900

    Overall redone

--- DBFP on GIT.R
+++ DBFP on GIT.R
@@ -20,123 +20,140 @@ load("DBFP.2003.2012.Rdata")
 ### Data cleansing
 #########################################################################################################################
 
-# Analysis type
-type <- 1 # 1: month-wise, 2: three-months average
-
-# Input/Output
-id.x.s1 <- c(13:16)
-id.y.s1 <- c(17:19)
-id.x.s2 <- c(17:18)
-id.y.s2 <- c(20:21)
-
-# Data for analysis
-if(type == 1){
-  df.2d <- df.raw
-}else{
-  df.2d <- data.frame()  
-  for(i in 202006:202012){
-    id.eff <- c()
-    for(j in df.raw[which(df.raw[,1] == i), 2]){
-      if(sum(df.raw[which(df.raw[,1] %in% c(i - 2, i - 1)), 2] == j) == 2){
-        id.eff <- c(id.eff, j)
-      }else{
-        next
-      }
-    }
-
-    df.temp.cr <- subset(df.raw, df.raw[,1] == i       & df.raw[,2] %in% id.eff)
-    df.temp.p1 <- subset(df.raw, df.raw[,1] == (i - 1) & df.raw[,2] %in% id.eff)
-    df.temp.p2 <- subset(df.raw, df.raw[,1] == (i - 2) & df.raw[,2] %in% id.eff)
-    df.temp.cr <- df.temp.cr[order(df.temp.cr[,2]),]
-    df.temp.p1 <- df.temp.p1[order(df.temp.p1[,2]),]
-    df.temp.p2 <- df.temp.p2[order(df.temp.p2[,2]),]
-    
-    df.temp.cr[, c(id.x.s1, id.y.s1, id.y.s2)] <- 
-      df.temp.cr[, c(id.x.s1, id.y.s1, id.y.s2)] + 
-      df.temp.p1[, c(id.x.s1, id.y.s1, id.y.s2)] +
-      df.temp.p2[, c(id.x.s1, id.y.s1, id.y.s2)]
-    
-    df.2d <- rbind(df.2d, df.temp.cr)
-  }
-}
-
 # IDs for exclusion
-id.out.level  <- which( is.na(match(df.2d[,3], c(100, 101, 102, 109, 112))))
-id.out.branch <- which(!is.na(match(df.2d[,5], c(8262, 8312, 8335, 8354))))
-id.out.bunit  <- which(!is.na(match(df.2d[,4], c(74, NA))))
-id.out.month  <- which(df.2d[,10] == 1)
-
-# Zero output
-id.out.zero.s1 <- which(rowSums(df.2d[,c(17, 18)]) == 0)
-#id.out.zero.s2 <- which(rowSums(df.2d[,c(20, 21)]) == 0)
-
-# Month of interest
-moi        <- 202006
-id.out.moe <- which(df.2d[,1] != moi)
+id.out.level   <- which( is.na(match(df.raw[,3], c(100, 101, 102, 109, 112))))
+id.out.branch  <- which(!is.na(match(df.raw[,5], c(8262, 8312, 8335, 8354))))
+id.out.bunit   <- which(!is.na(match(df.raw[,4], c(74, NA))))
+id.out.month   <- which(df.raw[,10] == 1)
+id.out.zero.s1 <- which(rowSums(df.raw[,c(17, 18)]) == 0)
 
 # Effective data
-id.out.all <- unique(c(id.out.level, id.out.branch, id.out.bunit, id.out.month,
-                       id.out.zero.s1, id.out.moe))
-df.eff     <- df.2d[-id.out.all,]
+id.out.all <- unique(c(id.out.level, id.out.branch, id.out.bunit, id.out.month, id.out.zero.s1))
+df.temp    <- df.raw[-id.out.all,]
+df.eff.mw  <- df.temp[order(df.temp[,1], df.temp[,2]),]
 
-# Class
-id.nb      <- which(df.eff[,9] == 1)
-id.ob      <- which(df.eff[,9] == 0)
-id.m.02.04 <- which(df.eff[,10] > 1 & df.eff[,10] < 5)
-id.m.05.07 <- which(df.eff[,10] > 4 & df.eff[,10] < 8)
-id.m.08.13 <- which(df.eff[,10] > 7 & df.eff[,10] < 14)
-id.m.14.00 <- which(df.eff[,10] > 13)
-
-# Count
-length(intersect(id.nb, id.m.02.04))
-length(intersect(id.nb, id.m.05.07))
-length(intersect(id.nb, id.m.08.13))
-length(intersect(id.nb, id.m.14.00))
-length(intersect(id.ob, id.m.02.04))
-length(intersect(id.ob, id.m.05.07))
-length(intersect(id.ob, id.m.08.13))
-length(intersect(id.ob, id.m.14.00))
+# DF for sliding average
+df.eff.sa <- data.frame()  
+for(i in 202006:202012){
+  id.eff <- c()
+  for(j in df.eff.mw[which(df.eff.mw[,1] == i), 2]){
+    if(sum(df.eff.mw[which(df.eff.mw[,1] %in% c(i - 2, i - 1)), 2] == j) == 2){
+      id.eff <- c(id.eff, j)
+    }else{
+      next
+    }
+  }
+  df.temp.cr <- subset(df.eff.mw, df.eff.mw[,1] == i       & df.eff.mw[,2] %in% id.eff)
+  df.temp.p1 <- subset(df.eff.mw, df.eff.mw[,1] == (i - 1) & df.eff.mw[,2] %in% id.eff)
+  df.temp.p2 <- subset(df.eff.mw, df.eff.mw[,1] == (i - 2) & df.eff.mw[,2] %in% id.eff)
+  df.temp.cr <- df.temp.cr[order(df.temp.cr[,2]),]
+  df.temp.p1 <- df.temp.p1[order(df.temp.p1[,2]),]
+  df.temp.p2 <- df.temp.p2[order(df.temp.p2[,2]),]
+  df.temp    <- df.temp.cr
+  df.temp[,13:21] <- df.temp.cr[,13:21] + df.temp.p1[,13:21] + df.temp.p2[,13:21]
+  df.eff.sa  <- rbind(df.eff.sa, df.temp)
+}
 
 
 #########################################################################################################################
 ### FP Evaluation
 #########################################################################################################################
 
+# Month of interest
+m        <- 202012
+df.fp.mw <- df.eff.mw[df.eff.mw[,1] == m,]
+df.fp.sa <- df.eff.sa[df.eff.sa[,1] == m,]
+
+# Only consider the same FP for mw
+df.fp.mw <- df.fp.mw[df.fp.mw[,2] %in% df.fp.sa[,2],]
+
+# IDs
+id.x.s1 <- c(13:16)
+id.y.s1 <- c(17:19)
+id.x.s2 <- c(17:18)
+id.y.s2 <- c(20:21)
+id.m.02.04.mw <- which(df.fp.mw[,10] < 5)
+id.m.05.07.mw <- which(df.fp.mw[,10] > 4 & df.fp.mw[,10] < 8)
+id.m.08.13.mw <- which(df.fp.mw[,10] > 7 & df.fp.mw[,10] < 14)
+id.m.14.00.mw <- which(df.fp.mw[,10] > 13)
+id.m.02.04.sa <- which(df.fp.sa[,10] < 5)
+id.m.05.07.sa <- which(df.fp.sa[,10] > 4 & df.fp.sa[,10] < 8)
+id.m.08.13.sa <- which(df.fp.sa[,10] > 7 & df.fp.sa[,10] < 14)
+id.m.14.00.sa <- which(df.fp.sa[,10] > 13)
+
 # Productivity analysis
-res.eff.fp <- data.frame(); res.s1.l <- res.s2.l <- list()
+res.eff.fp.mw <- res.eff.fp.sa <- data.frame(); res.s1.l.mw <- res.s2.l.mw <- res.s1.l.sa <- res.s2.l.sa <- list()
 for(i in 1:2){
   for(j in 1:4){
-    id.type    <- if(i == 1) id.nb else id.ob
-    id.month   <- if(j == 1) id.m.02.04 else if(j == 2) id.m.05.07 else if(j == 3) id.m.08.13 else id.m.14.00
-    id.eff     <- intersect(id.type, id.month)
-    adj.min    <- apply(df.eff[id.eff, id.y.s2], 2, function(x) if(min(x) < 0) -min(x) else 0)
-    df.temp    <- df.eff[id.eff, ]; df.temp[,id.y.s2] <- t(t(df.temp[,id.y.s2]) + adj.min)
-    g          <- cbind(matrix(rep(0, length(id.eff) * length(id.x.s1)), ncol = length(id.x.s1)), df.temp[,id.y.s1])
-    wd         <- c(0, 0, 1)
-    id.calc    <- which(rowSums(df.temp[,id.y.s2]) > 0)
-    xdata.s1   <- df.temp[,id.x.s1]
-    ydata.s1   <- df.temp[,id.y.s1]
-    xdata.s2   <- df.temp[,id.x.s2]
-    ydata.s2   <- df.temp[,id.y.s2]
-    res.s1     <- dm.sf (xdata.s1, ydata.s1, "vrs", g, wd)
-    res.s2     <- dm.dea(xdata.s2, ydata.s2, "vrs", "o", o = id.calc)
-    res.s1.l   <- list(res.s1.l, res.s1$lambda)
-    res.s2.l   <- list(res.s2.l, res.s2$lambda)
-    res.eff.fp <- rbind(res.eff.fp,
-                        data.frame(FP.type  = i,
-                                   FP.month = j,
-                                   FP.id    = df.temp[,2],
-                                   Eff.s1   = res.s1$eff + 1,
-                                   Eff.s2   = res.s2$eff))
+    id.type.mw    <- if(i == 1) which(df.fp.mw[,9] == 1) else which(df.fp.mw[,9] == 0)
+    id.type.sa    <- if(i == 1) which(df.fp.sa[,9] == 1) else which(df.fp.sa[,9] == 0)
+    id.m.mw       <- if(j == 1) id.m.02.04.mw else if(j == 2) id.m.05.07.mw else if(j == 3) id.m.08.13.mw else id.m.14.00.mw
+    id.m.sa       <- if(j == 1) id.m.02.04.sa else if(j == 2) id.m.05.07.sa else if(j == 3) id.m.08.13.sa else id.m.14.00.sa
+    id.eff.mw     <- intersect(id.type.mw, id.m.mw)
+    id.eff.sa     <- intersect(id.type.sa, id.m.sa)
+    adj.min.mw    <- apply(df.fp.mw[id.eff.mw, id.y.s2], 2, function(x) if(min(x) < 0) -min(x) else 0)
+    adj.min.sa    <- apply(df.fp.sa[id.eff.sa, id.y.s2], 2, function(x) if(min(x) < 0) -min(x) else 0)
+    df.temp.mw    <- df.fp.mw[id.eff.mw,]; df.temp.mw[,id.y.s2] <- t(t(df.temp.mw[,id.y.s2]) + adj.min.mw)
+    df.temp.sa    <- df.fp.sa[id.eff.sa,]; df.temp.sa[,id.y.s2] <- t(t(df.temp.sa[,id.y.s2]) + adj.min.sa)
+    g.mw          <- cbind(matrix(rep(0, length(id.eff.mw) * length(id.x.s1)), ncol = length(id.x.s1)), df.temp.mw[,id.y.s1])
+    g.sa          <- cbind(matrix(rep(0, length(id.eff.sa) * length(id.x.s1)), ncol = length(id.x.s1)), df.temp.sa[,id.y.s1])
+    wd            <- c(0, 0, 1)
+    id.calc.mw    <- which(rowSums(df.temp.mw[,id.y.s2]) > 0)
+    id.calc.sa    <- which(rowSums(df.temp.sa[,id.y.s2]) > 0)
+    res.s1.mw     <- dm.sf (df.temp.mw[,id.x.s1], df.temp.mw[,id.y.s1], "vrs", g.mw, wd)
+    res.s1.sa     <- dm.sf (df.temp.sa[,id.x.s1], df.temp.sa[,id.y.s1], "vrs", g.sa, wd)
+    res.s2.mw     <- dm.dea(df.temp.mw[,id.x.s2], df.temp.mw[,id.y.s2], "vrs", "o", o = id.calc.mw)
+    res.s2.sa     <- dm.dea(df.temp.sa[,id.x.s2], df.temp.sa[,id.y.s2], "vrs", "o", o = id.calc.sa)
+    res.s1.l.mw   <- list(res.s1.l.mw, res.s1.mw$lambda)
+    res.s1.l.sa   <- list(res.s1.l.sa, res.s1.sa$lambda)
+    res.s2.l.mw   <- list(res.s2.l.mw, res.s2.mw$lambda)
+    res.s2.l.sa   <- list(res.s2.l.sa, res.s2.sa$lambda)
+    res.eff.fp.mw <- rbind(res.eff.fp.mw, data.frame(FP.type  = i,
+                                                     FP.month = j,
+                                                     FP.id    = df.temp.mw[,2],
+                                                     Closed.m = m,
+                                                     Eff.s1   = res.s1.mw$eff + 1,
+                                                     Eff.s2   = res.s2.mw$eff))
+    res.eff.fp.sa <- rbind(res.eff.fp.sa, data.frame(FP.type  = i,
+                                                     FP.month = j,
+                                                     FP.id    = df.temp.sa[,2],
+                                                     Closed.m = m,
+                                                     Eff.s1   = res.s1.sa$eff + 1,
+                                                     Eff.s2   = res.s2.sa$eff))
   }
 }
 
 # Cleanse lambda list
-res.lambda <- list(list(res.s1.l[[1]][[1]][[1]][[2]], res.s1.l[[1]][[1]][[2]], res.s1.l[[1]][[2]], res.s1.l[[2]]),
-                   list(res.s2.l[[1]][[1]][[1]][[2]], res.s2.l[[1]][[1]][[2]], res.s2.l[[1]][[2]], res.s2.l[[2]]))
+res.l.mw <- list(list(res.s1.l.mw[[1]][[1]][[1]][[2]], res.s1.l.mw[[1]][[1]][[2]], res.s1.l.mw[[1]][[2]], res.s1.l.mw[[2]]),
+                 list(res.s2.l.mw[[1]][[1]][[1]][[2]], res.s2.l.mw[[1]][[1]][[2]], res.s2.l.mw[[1]][[2]], res.s2.l.mw[[2]]))
+res.l.sa <- list(list(res.s1.l.sa[[1]][[1]][[1]][[2]], res.s1.l.sa[[1]][[1]][[2]], res.s1.l.sa[[1]][[2]], res.s1.l.sa[[2]]),
+                 list(res.s2.l.sa[[1]][[1]][[1]][[2]], res.s2.l.sa[[1]][[1]][[2]], res.s2.l.sa[[1]][[2]], res.s2.l.sa[[2]]))
 
-# List of efficient FP
-res.eff[which(rowSums(res.eff[,c(4:5)]) == 2),]
+# Comparison of the results
+res.eff <- data.frame(res.eff.fp.mw, res.eff.fp.sa[,5:6], 
+                      Diff.r.s1 = round(res.eff.fp.mw[,5] / res.eff.fp.sa[,5], 2),
+                      Diff.r.s2 = round(res.eff.fp.mw[,6] / res.eff.fp.sa[,6], 2))
+
+# Stats
+temp.s1 <- aggregate(res.eff[res.eff[,1] == 1, 5:8], list(res.eff[res.eff[,1] == 1, 2]), mean, na.rm = T)
+temp.s2 <- aggregate(res.eff[res.eff[,1] == 2, 5:8], list(res.eff[res.eff[,1] == 2, 2]), mean, na.rm = T)
+round(temp.s1[,2] - temp.s1[,4], 2);round(temp.s1[,3] - temp.s1[,5], 2)
+round(temp.s2[,2] - temp.s2[,4], 2);round(temp.s2[,3] - temp.s2[,5], 2)
+
+# Extract extreme differences
+res.eff[c(which(res.eff$Diff.r.s1 == min(res.eff$Diff.r.s1)), which(res.eff$Diff.r.s1 == max(res.eff$Diff.r.s1)),
+          which(res.eff$Diff.r.s2 == min(res.eff$Diff.r.s2, na.rm = T)), which(res.eff$Diff.r.s2 == max(res.eff$Diff.r.s2, na.rm = T))),]
+
+# Further investigation
+id.fp <- 840225; m <- 202009
+id.fp <-   2553; m <- 202010
+id.fp <- 850471; m <- 202010
+id.fp <- 871031; m <- 202009
+
+t(rbind(df.eff.mw[df.eff.mw[,1] == (m - 2) & df.eff.mw[,2] == id.fp, c(1, id.x.s1, id.y.s1, id.y.s2)],
+        df.eff.mw[df.eff.mw[,1] == (m - 1) & df.eff.mw[,2] == id.fp, c(1, id.x.s1, id.y.s1, id.y.s2)],
+        df.eff.mw[df.eff.mw[,1] == m       & df.eff.mw[,2] == id.fp, c(1, id.x.s1, id.y.s1, id.y.s2)],
+        df.eff.sa[df.eff.sa[,1] == m       & df.eff.sa[,2] == id.fp, c(1, id.x.s1, id.y.s1, id.y.s2)]))
 
 
 #########################################################################################################################
