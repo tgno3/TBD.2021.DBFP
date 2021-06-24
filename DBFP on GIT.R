d2fddf8ee19cc93d8c6499284956075672904928
tgno3 <tgno3.com@gmail.com>
2021-06-24 17:10:30 +0900

    Malmquist added

--- DBFP on GIT.R
+++ DBFP on GIT.R
@@ -9,7 +9,7 @@
 #########################################################################################################################
 
 # Load library
-pkgs <- c("DJL")
+pkgs <- c("DJL", "ggplot2")
 sapply(pkgs, require, character.only = T)
 
 # Load data & parameters
@@ -54,8 +54,11 @@ for(i in 202006:202012){
   df.eff.sa  <- rbind(df.eff.sa, df.temp)
 }
 
+# Average for comprehensive profit margin
+df.eff.sa[,20] <- df.eff.sa[,20]/3
+
 # Data of interest
-m        <- 202007
+m        <- 202006
 #df.fp.mw <- df.eff.mw[df.eff.mw[,1] == m,]
 df.fp.sa <- df.eff.sa[df.eff.sa[,1] == m,]
 
@@ -73,6 +76,7 @@ id.y.s2 <- c(20:21)
 ### FP Evaluation
 #########################################################################################################################
 
+# Sub-grouping
 # id.m.02.04.mw <- which(df.fp.mw[,10] < 5)
 # id.m.05.07.mw <- which(df.fp.mw[,10] > 4 & df.fp.mw[,10] < 8)
 # id.m.08.13.mw <- which(df.fp.mw[,10] > 7 & df.fp.mw[,10] < 14)
@@ -81,6 +85,35 @@ id.m.02.04.sa <- which(df.fp.sa[,10] < 5)
 id.m.05.07.sa <- which(df.fp.sa[,10] > 4 & df.fp.sa[,10] < 8)
 id.m.08.13.sa <- which(df.fp.sa[,10] > 7 & df.fp.sa[,10] < 14)
 id.m.14.00.sa <- which(df.fp.sa[,10] > 13)
+id.m.02.06.sa <- which(df.fp.sa[,10] <  7)
+id.m.07.12.sa <- which(df.fp.sa[,10] >  6 & df.fp.sa[,10] < 13)
+id.m.13.24.sa <- which(df.fp.sa[,10] > 12 & df.fp.sa[,10] < 25)
+id.m.25.00.sa <- which(df.fp.sa[,10] > 24)
+
+
+# Diff
+id.m.02.04.sa <- which(df.eff.sa[,10] < 5)
+id.m.05.07.sa <- which(df.eff.sa[,10] > 4 & df.eff.sa[,10] < 8)
+id.m.08.13.sa <- which(df.eff.sa[,10] > 7 & df.eff.sa[,10] < 14)
+id.m.14.00.sa <- which(df.eff.sa[,10] > 13)
+id.m.02.06.sa <- which(df.eff.sa[,10] <  7)
+id.m.07.12.sa <- which(df.eff.sa[,10] >  6 & df.eff.sa[,10] < 13)
+id.m.13.24.sa <- which(df.eff.sa[,10] > 12 & df.eff.sa[,10] < 25)
+id.m.25.00.sa <- which(df.eff.sa[,10] > 24)
+
+res.summary.old <- res.summary.new <- data.frame()
+for(i in 1:2){
+  for(j in 1:4){
+    id.type.sa  <- if(i == 1) which(df.eff.sa[,9] == 1) else which(df.eff.sa[,9] == 0)
+    id.m.sa.old <- if(j == 1) id.m.02.04.sa else if(j == 2) id.m.05.07.sa else if(j == 3) id.m.08.13.sa else id.m.14.00.sa
+    id.m.sa.new <- if(j == 1) id.m.02.06.sa else if(j == 2) id.m.07.12.sa else if(j == 3) id.m.13.24.sa else id.m.25.00.sa
+    res.summary.old <- rbind(res.summary.old, table(df.eff.sa[intersect(id.type.sa, id.m.sa.old), 1]))
+    res.summary.new <- rbind(res.summary.new, table(df.eff.sa[intersect(id.type.sa, id.m.sa.new), 1]))
+  }
+}
+names(res.summary.old) <- names(res.summary.new) <- 202006:202012
+data.frame(Old = res.summary.old, New = res.summary.new)
+
 
 # Productivity analysis
 res.eff.fp.mw <- res.eff.fp.sa <- data.frame(); res.s1.l.mw <- res.s2.l.mw <- res.s1.l.sa <- res.s2.l.sa <- res.s2.l.sa.se <- list()
@@ -103,7 +136,8 @@ for(i in 1:2){
   
     # 3 months sliding-average
     id.type.sa    <- if(i == 1) which(df.fp.sa[,9] == 1) else which(df.fp.sa[,9] == 0)
-    id.m.sa       <- if(j == 1) id.m.02.04.sa else if(j == 2) id.m.05.07.sa else if(j == 3) id.m.08.13.sa else id.m.14.00.sa
+    #id.m.sa       <- if(j == 1) id.m.02.04.sa else if(j == 2) id.m.05.07.sa else if(j == 3) id.m.08.13.sa else id.m.14.00.sa
+    id.m.sa       <- if(j == 1) id.m.02.06.sa else if(j == 2) id.m.07.12.sa else if(j == 3) id.m.13.24.sa else id.m.25.00.sa
     id.eff.sa     <- intersect(id.type.sa, id.m.sa)
     adj.min.sa    <- apply(df.fp.sa[id.eff.sa, id.y.s2], 2, function(x) if(min(x) < 0) -min(x) else 0)
     df.temp.sa    <- df.fp.sa[id.eff.sa,]; df.temp.sa[,id.y.s2] <- t(t(df.temp.sa[,id.y.s2]) + adj.min.sa)
@@ -185,6 +219,77 @@ stat.s2 <- data.frame(aggregate(res.eff.fp.sa$Eff.s2, list(res.eff.fp.sa$FP.type
                       n = aggregate(res.eff.fp.sa$Eff.s2, list(res.eff.fp.sa$FP.type, res.eff.fp.sa$FP.month), function(x) length(x))$x)
 stat.s1[order(stat.s1[,1], stat.s1[,2]),]
 stat.s2[order(stat.s2[,1], stat.s2[,2]),]
+
+
+# Loop for FP grouping changes
+res.sd.s1 <- res.sd.s2 <- data.frame()
+for(m in 202006:202012){
+  df.fp.sa          <- df.eff.sa[df.eff.sa[,1] == m,]
+  id.m.02.04.sa     <- which(df.fp.sa[,10] < 5)
+  id.m.05.07.sa     <- which(df.fp.sa[,10] > 4 & df.fp.sa[,10] < 8)
+  id.m.08.13.sa     <- which(df.fp.sa[,10] > 7 & df.fp.sa[,10] < 14)
+  id.m.14.00.sa     <- which(df.fp.sa[,10] > 13)
+  id.m.02.06.sa     <- which(df.fp.sa[,10] <  7)
+  id.m.07.12.sa     <- which(df.fp.sa[,10] >  6 & df.fp.sa[,10] < 13)
+  id.m.13.24.sa     <- which(df.fp.sa[,10] > 12 & df.fp.sa[,10] < 25)
+  id.m.25.00.sa     <- which(df.fp.sa[,10] > 24)
+  res.eff.fp.sa.old <- res.eff.fp.sa.new <- data.frame()
+  for(i in 1:2){
+    for(j in 1:4){
+      id.type.sa       <- if(i == 1) which(df.fp.sa[,9] == 1) else which(df.fp.sa[,9] == 0)
+      id.m.sa.old      <- if(j == 1) id.m.02.04.sa else if(j == 2) id.m.05.07.sa else if(j == 3) id.m.08.13.sa else id.m.14.00.sa
+      id.m.sa.new      <- if(j == 1) id.m.02.06.sa else if(j == 2) id.m.07.12.sa else if(j == 3) id.m.13.24.sa else id.m.25.00.sa
+      id.eff.sa.old    <- intersect(id.type.sa, id.m.sa.old)
+      id.eff.sa.new    <- intersect(id.type.sa, id.m.sa.new)
+      adj.min.sa.old   <- apply(df.fp.sa[id.eff.sa.old, id.y.s2], 2, function(x) if(min(x) < 0) -min(x) else 0)
+      adj.min.sa.new   <- apply(df.fp.sa[id.eff.sa.new, id.y.s2], 2, function(x) if(min(x) < 0) -min(x) else 0)
+      df.temp.sa.old   <- df.fp.sa[id.eff.sa.old,]; df.temp.sa.old[,id.y.s2] <- t(t(df.temp.sa.old[,id.y.s2]) + adj.min.sa.old)
+      df.temp.sa.new   <- df.fp.sa[id.eff.sa.new,]; df.temp.sa.new[,id.y.s2] <- t(t(df.temp.sa.new[,id.y.s2]) + adj.min.sa.new)
+      g.sa.old         <- cbind(matrix(rep(0, length(id.eff.sa.old) * length(id.x.s1)), ncol = length(id.x.s1)), df.temp.sa.old[,id.y.s1])
+      g.sa.new         <- cbind(matrix(rep(0, length(id.eff.sa.new) * length(id.x.s1)), ncol = length(id.x.s1)), df.temp.sa.new[,id.y.s1])
+      wd               <- c(0, 0, 1)
+      id.calc.sa.old   <- which(rowSums(df.temp.sa.old[,id.y.s2]) > 0)
+      id.calc.sa.new   <- which(rowSums(df.temp.sa.new[,id.y.s2]) > 0)
+      res.s1.sa.old    <- dm.sf (df.temp.sa.old[,id.x.s1], df.temp.sa.old[,id.y.s1], "vrs", g.sa.old, wd)
+      res.s1.sa.new    <- dm.sf (df.temp.sa.new[,id.x.s1], df.temp.sa.new[,id.y.s1], "vrs", g.sa.new, wd)
+      res.s1.sa.se.old <- dm.sf (df.temp.sa.old[,id.x.s1], df.temp.sa.old[,id.y.s1], "crs", g.sa.old, wd, se = T)
+      res.s1.sa.se.new <- dm.sf (df.temp.sa.new[,id.x.s1], df.temp.sa.new[,id.y.s1], "crs", g.sa.new, wd, se = T)
+      res.s2.sa.old    <- dm.dea(df.temp.sa.old[,id.x.s2], df.temp.sa.old[,id.y.s2], "vrs", "o", o = id.calc.sa.old)
+      res.s2.sa.new    <- dm.dea(df.temp.sa.new[,id.x.s2], df.temp.sa.new[,id.y.s2], "vrs", "o", o = id.calc.sa.new)
+      res.s2.sa.se.old <- dm.sbm(df.temp.sa.old[,id.x.s2], df.temp.sa.old[,id.y.s2], "vrs", o = id.calc.sa.old, se = T)
+      res.s2.sa.se.new <- dm.sbm(df.temp.sa.new[,id.x.s2], df.temp.sa.new[,id.y.s2], "vrs", o = id.calc.sa.new, se = T)
+
+      # Summary
+      res.eff.fp.sa.old <- rbind(res.eff.fp.sa.old, data.frame(FP.type   = i,
+                                                               FP.month  = j,
+                                                               FP.id     = df.temp.sa.old[,2],
+                                                               Closed.m  = m,
+                                                               Eff.s1    = round(100/(res.s1.sa.old$eff + 1)),
+                                                               Eff.s2    = round(100/(res.s2.sa.old$eff))))
+      res.eff.fp.sa.new <- rbind(res.eff.fp.sa.new, data.frame(FP.type   = i,
+                                                               FP.month  = j,
+                                                               FP.id     = df.temp.sa.new[,2],
+                                                               Closed.m  = m,
+                                                               Eff.s1    = round(100/(res.s1.sa.new$eff + 1)),
+                                                               Eff.s2    = round(100/(res.s2.sa.new$eff))))
+    }
+  }
+  
+  res.sd.s1.old <- aggregate(res.eff.fp.sa.old$Eff.s1, list(res.eff.fp.sa.old$FP.type, res.eff.fp.sa.old$FP.month), sd, na.rm = T)
+  res.sd.s1.new <- aggregate(res.eff.fp.sa.new$Eff.s1, list(res.eff.fp.sa.new$FP.type, res.eff.fp.sa.new$FP.month), sd, na.rm = T)
+  res.sd.s2.old <- aggregate(res.eff.fp.sa.old$Eff.s2, list(res.eff.fp.sa.old$FP.type, res.eff.fp.sa.old$FP.month), sd, na.rm = T)
+  res.sd.s2.new <- aggregate(res.eff.fp.sa.new$Eff.s2, list(res.eff.fp.sa.new$FP.type, res.eff.fp.sa.new$FP.month), sd, na.rm = T)
+  res.sd.s1 <- rbind(res.sd.s1, data.frame(Month  = rep(m, 8), Type = 1:8,
+                                           Before = round(res.sd.s1.old[order(res.sd.s1.old[,1], res.sd.s1.old[,2]), 3], 2),
+                                           After  = round(res.sd.s1.new[order(res.sd.s1.new[,1], res.sd.s1.new[,2]), 3], 2)))
+  res.sd.s2 <- rbind(res.sd.s2, data.frame(Month  = rep(m, 8), Type = 1:8,
+                                           Before = round(res.sd.s2.old[order(res.sd.s2.old[,1], res.sd.s2.old[,2]), 3], 2),
+                                           After  = round(res.sd.s2.new[order(res.sd.s2.new[,1], res.sd.s2.new[,2]), 3], 2)))
+}
+
+temp <- data.frame(S1 = stat.s1[order(stat.s1[,1], stat.s1[,2]),],
+                   S2 = stat.s2[order(stat.s2[,1], stat.s2[,2]),])
+data.frame(R = round(c(temp$S1.x/temp$S1.n, temp$S2.x/temp$S2.n)*100))
 
 # # Comparison of the results
 # res.eff <- data.frame(res.eff.fp.mw, res.eff.fp.sa[,5:6], 
@@ -272,8 +377,154 @@ res.eff.br.sa  <- data.frame(Brc.id  = df.temp[,1],
 
 df.temp[order(res.eff.br.sa$Rank.s1),]
 
-#########################################################################################################################
-### Network Analysis
-#########################################################################################################################
+
+############################################## Time-series analysis ##############################################
+# Branches lasted for 7 months
+id.7       <- which(apply(table(df.eff.sa[,1], df.eff.sa[,5]) > 0, 2, function(x) sum(x) == 7) == T)
+df.7       <- subset(df.eff.sa, df.eff.sa[,5] %in% names(id.7))
+df.br.7.2d <- aggregate(df.7[,c(id.x.s1, id.y.s1, id.y.s2)], list(df.7[,1], df.7[,5]), sum)
+df.br.7.3d <- simplify2array(by(df.br.7.2d, df.br.7.2d[,1], as.matrix))
+
+# Branch type: 1(incu), 2(nor.sm), 3(nor.lg)
+br.type <- c()
+for(i in 1:length(id.7)){
+  if(df.7[which(df.7[,5] == names(id.7)[i]), 6][1] == 1){
+    br.type <- c(br.type, 1)
+  }else if(df.7[which(df.7[,5] == names(id.7)[i]), 8][1] == 1){
+    br.type <- c(br.type, 2)
+  }else{
+    br.type <- c(br.type, 3)
+  }
+}
+
+# Parameters
+id.x.s1.m   <- id.x.s1 - 10
+id.y.s1.m   <- id.y.s1 - 10
+id.x.s2.m   <- id.y.s1.m[1:2]
+id.y.s2.m   <- id.y.s2 - 10
+tm          <- c(paste0("2020.0", 6:9), paste0("2020.", 10:12))
+dm.s1       <- "sf"
+dm.s2       <- "dea"
+rts.s1      <- "vrs" 
+rts.s2      <- "crs" # "drs"
+orientation <- "o"
+g.m         <- df.br.7.3d[,c(id.x.s1.m, id.y.s1.m),]; g.m[,id.x.s1.m - 2,] <- 0
+wd          <- c(0, 0, 1)
+
+# Special treatment for zero input in stage 2
+df.br.7.3d[6, id.x.s2.m, 2][1] <- df.br.7.3d[6, id.x.s2.m, 1][1]
+
+# Run Malmquist
+# Incu
+# res.mq.s1.inc <- roc.malmquist(df.br.7.3d[type == 1, id.x.s1.m,], df.br.7.3d[type == 1, id.y.s1.m,], tm, dm.s1, rts.s1, g = g.m[type == 1,,], wd = wd)
+# res.mq.s2.inc <- roc.malmquist(df.br.7.3d[type == 1, id.x.s2.m,], df.br.7.3d[type == 1, id.y.s2.m,], tm, dm.s2, rts.s2, orientation)
+
+# Nor.sm
+res.mq.s1.nsm <- roc.malmquist(df.br.7.3d[type == 2, id.x.s1.m,], df.br.7.3d[type == 2, id.y.s1.m,], tm, dm.s1, rts.s1, g = g.m[type == 2,,], wd = wd)
+res.mq.s2.nsm <- roc.malmquist(df.br.7.3d[type == 2, id.x.s2.m,], df.br.7.3d[type == 2, id.y.s2.m,], tm, dm.s2, rts.s2, orientation)
+
+# Nor.lg
+# res.mq.s1.nlg <- roc.malmquist(df.br.7.3d[type == 3, id.x.s1.m,], df.br.7.3d[type == 3, id.y.s1.m,], tm, dm.s1, rts.s1, g = g.m[type == 3,,], wd = wd)
+# res.mq.s2.nlg <- roc.malmquist(df.br.7.3d[type == 3, id.x.s2.m,], df.br.7.3d[type == 3, id.y.s2.m,], tm, dm.s2, rts.s2, orientation)
+
+# Visualize the results
+res.mq.s1.nsm.avg <- data.frame(Period = unique(res.mq.s1.nsm$cu$Period),
+                                CU     = aggregate(res.mq.s1.nsm$cu$CU, list(res.mq.s1.nsm$cu$Period), mean)$x,
+                                FS     = aggregate(res.mq.s1.nsm$fs$FS, list(res.mq.s1.nsm$fs$Period), mean)$x,
+                                MI     = aggregate(res.mq.s1.nsm$mi$MI, list(res.mq.s1.nsm$mi$Period), mean)$x)
+
+res.mq.s2.nsm.avg <- data.frame(Period = unique(res.mq.s2.nsm$cu$Period),
+                                CU     = aggregate(res.mq.s2.nsm$cu$CU, list(res.mq.s2.nsm$cu$Period), mean)$x,
+                                FS     = aggregate(res.mq.s2.nsm$fs$FS, list(res.mq.s2.nsm$fs$Period), mean)$x,
+                                MI     = aggregate(res.mq.s2.nsm$mi$MI, list(res.mq.s2.nsm$mi$Period), mean)$x)
+
+# DMU of interest
+data.frame(DMU   = 1:length(names(id.7)[which(type == 2)]),
+           Br.No = names(id.7)[which(type == 2)])
+id.doi   <- c(13, 20)
+df.s1.fs <- data.frame(subset(res.mq.s1.nsm$fs, DMU %in% id.doi),
+                       Name = names(id.7)[which(type == 2)][id.doi])
+df.s1.mi <- data.frame(subset(res.mq.s1.nsm$mi, DMU %in% id.doi),
+                       Name = names(id.7)[which(type == 2)][id.doi])
+df.s2.fs <- data.frame(subset(res.mq.s2.nsm$fs, DMU %in% id.doi),
+                       Name = names(id.7)[which(type == 2)][id.doi])
+df.s2.mi <- data.frame(subset(res.mq.s2.nsm$mi, DMU %in% id.doi),
+                       Name = names(id.7)[which(type == 2)][id.doi])
+
+
+############################################## Stage 1 ##############################################
+par(mfrow = c(1, 2))
+# FS
+ggplot(data = res.mq.s1.nsm$fs, aes(x = Period, y = 1/FS)) + 
+  geom_point(alpha = 0.2, size = 1.2) + theme_bw() + geom_hline(yintercept = 1.0, color = "red", size = 0.5) +
+  geom_line(data = res.mq.s1.nsm.avg, aes(x = Period, y = 1/FS, group = 1), size = 1) +
+  geom_line(data = df.s1.fs, aes(x = Period, y = 1/FS, group = Name, color = Name), size = 1.2) + 
+  scale_y_continuous(name = "Frontier Shift (Stage 1)") +
+  #scale_y_continuous(name = "Malmquist Index (MI)", limits = c(0.85, 1.4), breaks = seq(0.85, 1.4, 0.2)) +
+  theme(axis.title.x         = element_text(size = 14, colour = "gray35"),
+        axis.title.y         = element_text(size = 14, colour = "gray35"),
+        legend.title         = element_blank(),
+        legend.background    = element_rect(fill = "transparent", colour = "transparent"), 
+        legend.direction     = "horizontal", 
+        legend.justification = c(1, 1), legend.position = c(0.95, 0.993))
+# MI
+ggplot(data = res.mq.s1.nsm$mi, aes(x = Period, y = 1/MI)) + 
+  geom_point(alpha = 0.2, size = 1.2) + theme_bw() + geom_hline(yintercept = 1.0, color = "red", size = 0.5) +
+  geom_line(data = res.mq.s1.nsm.avg, aes(x = Period, y = 1/MI, group = 1), size = 1) +
+  geom_line(data = df.s1.mi, aes(x = Period, y = 1/MI, group = Name, color = Name), size = 1.2) + 
+  scale_y_continuous(name = "Malmquist Index (Stage 1)") +
+  #scale_y_continuous(name = "Malmquist Index (MI)", limits = c(0.85, 1.4), breaks = seq(0.85, 1.4, 0.2)) +
+  theme(axis.title.x         = element_text(size = 14, colour = "gray35"),
+        axis.title.y         = element_text(size = 14, colour = "gray35"),
+        legend.title         = element_blank(),
+        legend.background    = element_rect(fill = "transparent", colour = "transparent"), 
+        legend.direction     = "horizontal", 
+        legend.justification = c(1, 1), legend.position = c(0.95, 0.993))
+par(mfrow = c(1, 1))
+
+############################################## Stage 2 ##############################################
+# FS
+ggplot(data = res.mq.s2.nsm$fs, aes(x = Period, y = 1/FS)) + 
+  geom_point(alpha = 0.2, size = 1.2) + theme_bw() + geom_hline(yintercept = 1.0, color = "red", size = 0.5) +
+  geom_line(data = res.mq.s2.nsm.avg, aes(x = Period, y = 1/FS, group = 1), size = 1) +
+  geom_line(data = df.s2.fs, aes(x = Period, y = 1/FS, group = Name, color = Name), size = 1.2) + 
+  scale_y_continuous(name = "Frontier Shift (Stage 1)") +
+  #scale_y_continuous(name = "Malmquist Index (MI)", limits = c(0.85, 1.4), breaks = seq(0.85, 1.4, 0.2)) +
+  theme(axis.title.x         = element_text(size = 14, colour = "gray35"),
+        axis.title.y         = element_text(size = 14, colour = "gray35"),
+        legend.title         = element_blank(),
+        legend.background    = element_rect(fill = "transparent", colour = "transparent"), 
+        legend.direction     = "horizontal", 
+        legend.justification = c(1, 1), legend.position = c(0.95, 0.993))
+
+# MI
+ggplot(data = res.mq.s2.nsm$mi, aes(x = Period, y = 1/MI)) + 
+  geom_point(alpha = 0.2, size = 1.2) + theme_bw() + geom_hline(yintercept = 1.0, color = "red", size = 0.5) +
+  geom_line(data = res.mq.s2.nsm.avg, aes(x = Period, y = 1/MI, group = 1), size = 1) + 
+  geom_line(data = df.s2.mi, aes(x = Period, y = 1/MI, group = Name, color = Name), size = 1.2) + 
+  scale_y_continuous(name = "Malmquist Index (Stage 2)") +
+  #scale_y_continuous(name = "Malmquist Index (MI)", limits = c(0.85, 1.4), breaks = seq(0.85, 1.4, 0.2)) +
+  theme(axis.title.x         = element_text(size = 14, colour = "gray35"),
+        axis.title.y         = element_text(size = 14, colour = "gray35"),
+        legend.title         = element_blank(),
+        legend.background    = element_rect(fill = "transparent", colour = "transparent"), 
+        legend.direction     = "horizontal", 
+        legend.justification = c(1, 1), legend.position = c(0.95, 0.993))
+
+
+# Investigation
+from <- 202011
+to   <- from + 1
+name <- 7015
+df.from <- subset(df.br.7.2d, Group.1 == from)[br.type == 2,]
+df.to   <- subset(df.br.7.2d, Group.1 == to  )[br.type == 2,]
+
+df.br.7.3d[which(df.br.7.3d[,2,1] == name),,(from - 202005):(to - 202005)]
+100/(dm.sf(df.from[,id.x.s1.m], df.from[,id.y.s1.m], rts.s1, g.m[br.type == 2,,from - 202005], wd)$eff[which(df.from$Group.2 == name)] + 1)
+100/(dm.sf(df.to  [,id.x.s1.m], df.to  [,id.y.s1.m], rts.s1, g.m[br.type == 2,,to   - 202005], wd)$eff[which(df.to  $Group.2 == name)] + 1)
+
+100/(dm.dea(df.from[,id.x.s2.m], df.from[,id.y.s2.m], rts.s2, orientation)$eff[which(df.from$Group.2 == name)])
+100/(dm.dea(df.to  [,id.x.s2.m], df.to  [,id.y.s2.m], rts.s2, orientation)$eff[which(df.to  $Group.2 == name)])
+
 
 
